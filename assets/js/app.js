ec6d795ed9a3a57bfc530273e5fc8d1a50ec5863
alexisOclock <alexis@oclock.io>
2021-02-12 12:52:03 +0100

    ajout de cartes ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,16 +18,18 @@
 const app = {
   // fonction d'initialisation, lancée au chargement de la page
   init: function () {
-    app.formElement = document.querySelector('#addListForm');
-    app.modalElement = document.querySelector('#addListModal');
+    // je mémorise des élements pour plus tard
+    app.listFormElement = document.querySelector('#addListForm');
+    app.listModalElement = document.querySelector('#addListModal');
+    app.buttonElement = document.querySelector('#addListButton');
+    app.cardFormElement = document.querySelector('#addCardForm');
+    app.cardModalElement = document.querySelector('#addCardModal');
     app.addListenerToActions();
   },
 
   addListenerToActions: function() { 
-    // cibler le bouton
-    const buttonElement = document.querySelector('#addListButton');
-    // poser un écouteur au click dessus
-    buttonElement.addEventListener('click', app.showAddListModal);
+    // poser un écouteur au click sur le botuon
+    app.buttonElement.addEventListener('click', app.showAddListModal);
     // cibler les boutons close
     const closeButtons = document.querySelectorAll('.close');
     // poser un écouteur dessus au click
@@ -39,7 +41,16 @@ const app = {
     });
     // objectif : afficher une liste dans la page à la validation du formulaire
     // réagir à la soumission
-    app.formElement.addEventListener('submit', app.handleAddListForm);
+    app.listFormElement.addEventListener('submit', app.handleAddListForm);
+
+    // on cible les + pour l'ajout de cartes
+    const cardButtonElements = document.querySelectorAll('.panel-heading a');
+    cardButtonElements.forEach((button) => {
+      button.addEventListener('click', app.showAddCardModal);
+    });
+
+    // soumission du formulaire des card
+    app.cardFormElement.addEventListener('submit', app.handleAddCardForm);
   },
 
   handleAddListForm: function(event) {
@@ -47,17 +58,30 @@ const app = {
     event.preventDefault(); 
     // récupérer la valeur du champ
     // grace au constructeur FormData je peux passer à la moulinette un form et ses champs, pour voir ensuite lire facilement les valeurs des champs
-    const data = new FormData(app.formElement);
+    const data = new FormData(app.listFormElement);
     // l'objet de data construit par FormData possède une méthode get permettant de récupérer la valeur d'un champ en fonction de son nom (son attribut name)
     const inputValue = data.get('listName');
     // créer une liste dans le DOM avec la valeur du champ
     app.makeListInDOM(inputValue);
+    // je vide le champ pour les prochaines fois
+    app.listModalElement.querySelector('input').value = '';
     // je ferme la modale
     app.hideModals();
   },
 
+  handleAddCardForm: function(event) {
+    // ajout de la card sur le meme modèle que la liste
+    event.preventDefault(); 
+    const data = new FormData(app.cardFormElement);
+    const inputValue = data.get('cardName');
+    // on récvupère la valeur du champ caché
+    const listId = data.get('listId');
+    app.makeCardInDOM(inputValue, listId);
+    app.cardModalElement.querySelector('input').value = '';
+    app.hideModals();
+  },
+
   makeListInDOM: function(listName) {
-    console.log('je crée la liste qui s\'appelle ' + listName);
     // je cible mon template
     const template = document.querySelector('#listTemplate');
     // je clone son contenu
@@ -68,24 +92,53 @@ const app = {
     title.textContent = listName;
     const panel = clone.querySelector('.panel');
     panel.setAttribute('data-list-id', 'X');
-    // j'injecte le clone dans ma page
-    // document.querySelector('.columns').appendChild(clone);
-    // cibler le bouton
-    const btn = document.querySelector('#addListButton');
-    // trouver son parent column
-    const column = btn.closest('.column');
+    // /!\ on écoute le click sur le + de la nouvelle liste aussi !
+    clone.querySelector('.panel-heading a').addEventListener('click', app.showAddCardModal);
+    // trouver le parent column du bouton
+    const column = app.buttonElement.closest('.column');
     // injecter avant le clone, on connait appendChild qui insère un enfant à la fin d'un parent. .before insère notre enfant à coté et juste avant un élement cible
     column.before(clone);
   },
 
+  // on prévoit ici un deuxième paramètre représentant l'id de la liste dans laquelle on veut la carte
+  makeCardInDOM: function(cardName, parentId) {
+    // création de la card dans le dom sur le meme principe que la liste
+    const template = document.querySelector('#cardTemplate');
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('.card-name').textContent = cardName;
+    clone.querySelector('.box').setAttribute('data-card-id', '?');
+    // on insère la card au bon endroit dans la liste, on utilise ici un selecteur d'attribut
+    document.querySelector(`div[data-list-id="${parentId}"] .panel-block`).appendChild(clone);
+  },
+
   hideModals: function() {
-    app.modalElement.classList.remove('is-active');
+    app.listModalElement.classList.remove('is-active');
+    app.cardModalElement.classList.remove('is-active');
+
+    // ou bien
+    // const modals = document.querySelectorAll('.modal');
+    // modals.forEach((modal) => {
+    //   modal.classList.remove('is-active');
+    // });
   },
 
   showAddListModal: function() {
     // modifier son style display
-    // modalElement.style.display = 'block';
-    app.modalElement.classList.add('is-active');
+    // listModalElement.style.display = 'block';
+    app.listModalElement.classList.add('is-active');
+  },
+
+  showAddCardModal: function(event) {
+    app.cardModalElement.classList.add('is-active');
+    // en plus d'afficher la modal, on modifie la valeur du champ caché pour mémoriser l'id de la liste parent
+    // on trouve le bouton cliqué
+    const btn = event.target;
+    // on trouve le parent représentant la liste
+    const parent = btn.closest('.panel');
+    // on trouve son list-id
+    const id = parent.getAttribute('data-list-id');
+    // on modifie la valeur du champ caché
+    app.cardModalElement.querySelector('input[name="listId"]').value = id;
   },
 
 };
