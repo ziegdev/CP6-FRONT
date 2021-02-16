f64cc512aa2ddaf8a2b950a1a3ab76ca3e224167
alexisOclock <alexis@oclock.io>
2021-02-16 11:26:40 +0100

    ajout ok

--- assets/js/card.js
+++ assets/js/card.js
@@ -4,21 +4,45 @@ const card = {
     card.modalElement = document.querySelector('#addCardModal');
   },
 
-  handleAddForm: function(event) {
+  handleAddForm: async function(event) {
     // ajout de la card sur le meme modèle que la liste
     event.preventDefault(); 
     const data = new FormData(card.formElement);
-    const inputValue = data.get('cardName');
     // on récvupère la valeur du champ caché
-    const listId = data.get('listId');
-    card.makeInDOM(inputValue, listId);
-    card.modalElement.querySelector('input').value = '';
-    util.hideModals();
+    const listId = data.get('list_id');
+    // permet d'ajouter une donnée aux formData
+    // on cible la liste
+    const listParent = document.querySelector(`.panel[data-list-id="${listId}"]`);
+    // on cible les cartes dedans
+    const boxes = listParent.querySelectorAll('.box');
+    // on compte pour connaitre la posisition de la nouvelle carte
+    data.append('position', boxes.length + 1);
+    try {
+      // avant de modifier le DOM on informe notre API qu'il faut faire persister des infos d'une carte
+      const response = await fetch(`${util.base_url}/card`, {
+        method: 'POST',
+        body: data,
+      });
+      const body = await response.json();
+      // si tout va bien
+      if (response.status === 200) {
+        // on modifie le dom
+        card.makeInDOM(body.card);
+        card.modalElement.querySelector('input').value = '';
+        util.hideModals();
+      }
+      else {
+        // sinon on gère l'erreur
+        throw new Error(body);
+      }
+    } catch(error) {
+      alert('Erreur lors de la sauvegarde de la carte');
+      console.error(error);
+    }
   },
 
   // on récupère un objet représentant la card
   makeInDOM: function(cardItem) {
-    console.log(cardItem);
     // création de la card dans le dom sur le meme principe que la liste
     const template = document.querySelector('#cardTemplate');
     const clone = template.content.cloneNode(true);
@@ -31,16 +55,22 @@ const card = {
   },
 
   showAddModal: function(event) {
+    event.preventDefault();
     card.modalElement.classList.add('is-active');
     // en plus d'afficher la modal, on modifie la valeur du champ caché pour mémoriser l'id de la liste parent
     // on trouve le bouton cliqué
     const btn = event.target;
+    // l'élement qui a été cliqué, celui qui a déclenché l'événement
+    // un événement en javascript se propage, donc un parent peut entendre les événement déclenchés sur ses enfants
+    // console.log(event.target)
+    // l'élement sur lequel l'événement est écouté
+    // console.log(event.currentTarget)
     // on trouve le parent représentant la liste
     const parent = btn.closest('.panel');
     // on trouve son list-id
     const id = parent.getAttribute('data-list-id');
     // on modifie la valeur du champ caché
-    card.modalElement.querySelector('input[name="listId"]').value = id;
+    card.modalElement.querySelector('input[name="list_id"]').value = id;
   },
 
 };
